f1f360ed2e0e9420747c39c0914336a75d2ef5a5
Rishab Ramanathan <rrishab2001@gmail.com>
2020-08-13 14:47:39 +0530

    wtf is javascript

--- js/test.js
+++ js/test.js
@@ -1,18 +1,19 @@
 function listing(arr)
 {
+    arr = [a,b,c,d];
     var i,len,text;
-    text = "<ul>"
-    len = arr.text
+    text = "<ul>";
+    len = arr.text;
     for(i = 0;i<len;i++)
     {
-        text += "<li>" + arr[i] + "</li>"
+        text += "<li>" + arr[i] + "</li>";
     }
-    text += "<ul>"
-    return text
+    text += "<ul>";
+    document.getElementById("test").innerHTML = text;
 }
 function test()
 {
-    var arr = ["ur","momma","hella","gay"]
-    var text = listing(arr)
-    document.getElementById("test").innerHTML = text
+    
+    var text = listing(arr);
+    
 }
